7684130c9b82dc12dd08ac35a0928409fe812af5
yjh0410 <1394571815@qq.com>
2021-12-09 15:38:50 +0800

    update

--- models/yolof.py
+++ models/yolof.py
@@ -238,7 +238,7 @@ class YOLOF(nn.Module):
 
             outputs = {"pred_cls": normalized_cls_pred,
                         "pred_box": box_pred,
-                        "mask": mask}
+                        "mask": None}
             return outputs 
 
 
