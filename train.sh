981d01b606e3503f468eb0a533566755bbf1db96
yjh0410 <1394571815@qq.com>
2021-12-10 22:04:16 +0800

    fix a bug in train.py

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         -v yolof_r50_C5_1x \
-        --batch_size 8 \
+        --batch_size 12 \
         --img_size 512 \
         --lr 0.01 \
         --norm GN \
         --wp_iter 1500 \
-        --accumulate 8
+        --accumulate 5
