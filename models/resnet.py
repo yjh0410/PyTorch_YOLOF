7dafab401ca0eba2de570fcb729c8e1d7d59b151
yjh0410 <1394571815@qq.com>
2021-12-10 21:51:24 +0800

    fix a bug in train.py

--- models/resnet.py
+++ models/resnet.py
@@ -154,6 +154,8 @@ class ResNet(nn.Module):
             p.requires_grad = False
         for p in self.bn1.parameters():
             p.requires_grad = False
+        for p in self.layer1.parameters():
+            p.requires_grad = False
 
     def forward(self, x):
         x = self.conv1(x)
