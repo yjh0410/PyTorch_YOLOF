8d9a2d24c4847028c71a7a3f6cb73ac4afff81a0
yjh0410 <1394571815@qq.com>
2021-12-10 15:19:13 +0800

    update

--- train.sh
+++ train.sh
@@ -2,6 +2,7 @@ python train.py \
         --cuda \
         -d voc \
         -v yolof_r50_C5_1x \
-        --batch_size 4 \
+        --batch_size 2 \
+        --img_size 800 \
         --norm GN \
         --accumulate 4
