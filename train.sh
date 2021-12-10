56e47be3e619d2af8a68a31aa9b8be24031ddf0c
yjh0410 <1394571815@qq.com>
2021-12-10 22:27:31 +0800

    update

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
         --cuda \
         -d coco \
         -v yolof_r50_C5_1x \
-        --batch_size 4 \
+        --batch_size 8 \
         --img_size 800 \
         --lr 0.01 \
         --norm GN \
