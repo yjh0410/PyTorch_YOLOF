78afa0245850615cb8785fe0b42416b9fafc0642
yjh0410 <1394571815@qq.com>
2021-12-11 10:05:46 +0800

    update

--- train_ddp.sh
+++ train_ddp.sh
@@ -8,8 +8,8 @@ python -m torch.distributed.launch --nproc_per_node=8 --use_env \
                         --batch_size 2 \
                         --img_size 800 \
                         --lr 0.01 \
-                        --norm GN \
                         --wp_iter 1500 \
                         --dist \
+                        --sybn \
                         --num_gpu 8 \
-                        --accumulate 32
+                        --accumulate 1
