7de641477dfbd0ba2debe764b468be281642bfef
yjh0410 <1394571815@qq.com>
2021-12-11 09:59:25 +0800

    update

--- models/resnet.py
+++ models/resnet.py
@@ -155,8 +155,8 @@ class ResNet(nn.Module):
         for p in self.bn1.parameters():
             p.requires_grad = False
 
-        # for p in self.layer1.parameters():
-        #     p.requires_grad = False
+        for p in self.layer1.parameters():
+            p.requires_grad = False
 
     def forward(self, x):
         x = self.conv1(x)
