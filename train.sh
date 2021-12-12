ffd35ac10331233a3e1239b42b4265d01ea9b1ee
yjh0410 <1394571815@qq.com>
2021-12-12 10:47:05 +0800

    add lr backbone

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ python train.py \
         -d coco \
         -v yolof_r50_C5_1x \
         --batch_size 16 \
-        --img_size 800 \
+        --img_size 512 \
         --lr 0.03 \
         --lr_backbone 0.01 \
-        --wp_iter 500 \
+        --wp_iter 1500 \
         --accumulate 1
