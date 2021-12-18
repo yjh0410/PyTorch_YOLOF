9c93bd5bd6c74627b154bc20dc76fc65bdc9faf1
yjh0410 <1394571815@qq.com>
2021-12-18 10:16:58 +0800

    update

--- train.sh
+++ train.sh
@@ -2,11 +2,10 @@ python train.py \
         --cuda \
         -d coco \
         -v yolof_r50_C5_1x \
-        --batch_size 16 \
+        --batch_size 15 \
         --img_size 800 \
         --lr 0.01 \
         --lr_backbone 0.01 \
         --wp_iter 500 \
-        --optimizer sgd \
-        --accumulate 1 \
-        --no_warmup
+        --optimizer adamw \
+        --accumulate 1
