c796afb7505070e85c37d2b09e62a4ddeca5a508
yjh0410 <1394571815@qq.com>
2021-12-19 21:33:36 +0800

    fix a bug in dataloader

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@ python train.py \
         --cuda \
         -d coco \
         -v yolof_r50_C5_1x \
-        --norm GN \
-        --batch_size 15 \
+        --norm BN \
+        --batch_size 16 \
         --img_size 800 \
         --lr 0.01 \
         --lr_backbone 0.01 \
         --wp_iter 500 \
         --optimizer sgd \
-        --accumulate 4
+        --accumulate 1
