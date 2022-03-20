c9918f7ba05ef85bec3071ebeb997b7d98db258e
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-20 09:28:09 +0800

    train YOLOF-R50-DC5-3x

--- train.sh
+++ train.sh
@@ -2,13 +2,13 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolof18 \
+        -v yolof50-DC5 \
         -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --train_min_size 800 \
-        --train_max_size 1333 \
-        --val_min_size 800 \
-        --val_max_size 1333 \
-        --schedule 1x \
+        --train_min_size 640 \
+        --train_max_size 640 \
+        --val_min_size 640 \
+        --val_max_size 640 \
+        --schedule 3x \
         --grad_clip_norm 4.0 \
