c2855849cb154c3f28da688ae8c919f66e1ac58f
yjh0410 <1394571815@qq.com>
2022-09-23 09:24:26 +0800

    update

--- train_ddp.sh
+++ train_ddp.sh
@@ -2,10 +2,10 @@
 python -m torch.distributed.run --nproc_per_node=2 train.py \
                                                     --cuda \
                                                     -dist \
-                                                    -d voc \
+                                                    -d coco \
                                                     --root /mnt/share/ssd2/dataset/ \
                                                     -v yolof18 \
-                                                    --batch_size 1 \
+                                                    --batch_size 8 \
                                                     --grad_clip_norm 4.0 \
                                                     --num_workers 4 \
                                                     --schedule 1x \
