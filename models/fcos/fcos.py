5de0f1d21a8ade8f56cfe06ce245edc28fe7d138
yjh0410 <1394571815@qq.com>
2022-09-30 14:53:56 +0800

    test fcos-r18

--- models/fcos/fcos.py
+++ models/fcos/fcos.py
@@ -142,7 +142,7 @@ class FCOS(nn.Module):
         
         for cls_pred_i, reg_pred_i, ctn_pred_i, anchors_i in zip(cls_pred, reg_pred, ctn_pred, anchors):
             # (H x W x C,)
-            cls_pred_i = (cls_pred_i.sigmoid() * ctn_pred_i.sigmoid()).flatten()
+            cls_pred_i = (torch.sqrt(cls_pred_i.sigmoid() * ctn_pred_i.sigmoid())).flatten()
 
             # Keep top k top scoring indices only.
             num_topk = min(self.topk, reg_pred_i.size(0))
