421329596c42c14e9da598ae9969d0bc7c70265b
yjh0410 <1394571815@qq.com>
2022-10-04 16:35:49 +0800

    train fcos-r50

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos-r50 \
-        --batch_size 8 \
+        -v fcos-r18 \
+        --batch_size 16 \
         --schedule 1x \
         --eval_epoch 1 \
         --grad_clip_norm 4.0 \
