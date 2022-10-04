198310219faf8bfa8c355df99b0ecd4df0a5a7cd
yjh0410 <1394571815@qq.com>
2022-10-04 15:56:25 +0800

    update

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos-r50-RT \
+        -v fcos-r18 \
         --batch_size 16 \
-        --schedule 3x \
+        --schedule 1x \
         --eval_epoch 1 \
         --grad_clip_norm 4.0 \
