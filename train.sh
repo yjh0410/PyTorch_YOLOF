08fbb408c7ec855b371cd264ce304819997330bb
yjh0410 <1394571815@qq.com>
2022-12-25 09:13:53 +0800

    debug YOLOF-Lite

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v retinanet-rt-r18 \
+        -v yolof-lite-r18 \
         --batch_size 16 \
-        --schedule 4x \
+        --schedule 1x \
         --eval_epoch 2 \
         --grad_clip_norm 4.0 \
